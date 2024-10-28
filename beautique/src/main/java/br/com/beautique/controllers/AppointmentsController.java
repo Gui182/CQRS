42d72136ec1103494b120f0f550ccd66f9aa26e3
Gui182 <guilherme.oliveira182@yahoo.com.br>
2024-10-28 19:57:38 -0300

    feat: Appointments Controller

--- beautique/src/main/java/br/com/beautique/controllers/AppointmentsController.java
+++ beautique/src/main/java/br/com/beautique/controllers/AppointmentsController.java
@@ -7,7 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.beautique.dtos.AppointmentDTO;
 import br.com.beautique.services.AppointmentsService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -20,6 +25,22 @@ public class AppointmentsController {
     @PostMapping
     ResponseEntity<AppointmentDTO> create(@RequestBody AppointmentDTO appointmentDTO) {
         return ResponseEntity.ok(appointmentsService.create(appointmentDTO));
+    }
+
+    @PatchMapping
+    ResponseEntity<AppointmentDTO> update(@RequestBody AppointmentDTO appointmentDTO) {
+        return ResponseEntity.ok(appointmentsService.update(appointmentDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        appointmentsService.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    ResponseEntity<AppointmentDTO> setCustomerToAppointment(@RequestBody AppointmentDTO appointmentDTO) {
+        return ResponseEntity.ok(appointmentsService.setCustomerToAppointment(appointmentDTO));
     }
 
 }
--- beautique/src/main/java/br/com/beautique/services/impl/AppointmentsServiceImpl.java
+++ beautique/src/main/java/br/com/beautique/services/impl/AppointmentsServiceImpl.java
@@ -1,11 +1,17 @@
 package br.com.beautique.services.impl;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.beautique.dtos.AppointmentDTO;
 import br.com.beautique.entities.AppointmentsEntity;
+import br.com.beautique.entities.BeautyProceduresEntity;
+import br.com.beautique.entities.CustomerEntity;
 import br.com.beautique.repositories.AppointmentRepository;
+import br.com.beautique.repositories.BeautyProcedureRepository;
+import br.com.beautique.repositories.CustomerRepository;
 import br.com.beautique.services.AppointmentsService;
 import br.com.beautique.utils.ConverterUtil;
 
@@ -15,32 +21,75 @@ public class AppointmentsServiceImpl implements AppointmentsService {
     @Autowired
     private AppointmentRepository appointmentRepository;
 
+    @Autowired
+    private BeautyProcedureRepository beautyProcedureRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
     private final ConverterUtil<AppointmentsEntity, AppointmentDTO> converterUtil = new ConverterUtil<>(
             AppointmentsEntity.class, AppointmentDTO.class);
 
     @Override
     public AppointmentDTO create(AppointmentDTO aaAppointmentDTO) {
-        AppointmentsEntity appointmentsEntity  = converterUtil.convertToSource(aaAppointmentDTO);
+        AppointmentsEntity appointmentsEntity = converterUtil.convertToSource(aaAppointmentDTO);
         AppointmentsEntity newAppointmentsEntity = appointmentRepository.save(appointmentsEntity);
         return converterUtil.convertToTarget(newAppointmentsEntity);
     }
 
     @Override
     public AppointmentDTO update(AppointmentDTO appointmentDTO) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        Optional<AppointmentsEntity> currentAppointment = appointmentRepository.findById(appointmentDTO.getId());
+        if (currentAppointment.isEmpty()) {
+            throw new RuntimeException("Appointment not found");
+        }
+        AppointmentsEntity appointmentsEntity = converterUtil.convertToSource(appointmentDTO);
+        appointmentsEntity.setCreatedAt(currentAppointment.get().getCreatedAt());
+        AppointmentsEntity updateAppointmentsEntity = appointmentRepository.save(appointmentsEntity);
+        return converterUtil.convertToTarget(updateAppointmentsEntity);
     }
 
     @Override
     public void deleteById(Long Id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+        AppointmentsEntity appointmentsEntity = appointmentRepository.findById(Id)
+                .orElseThrow(() -> new RuntimeException("Appointment not found"));
+        appointmentRepository.delete(appointmentsEntity);
     }
 
     @Override
     public AppointmentDTO setCustomerToAppointment(AppointmentDTO appointmentDTO) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setCustomerToAppointment'");
+        CustomerEntity customerEntity = findCustomerById(appointmentDTO.getCustomer());
+        BeautyProceduresEntity beautyProceduresEntity = findBeautyProceduresById(appointmentDTO.getBeautyProcedure());
+        AppointmentsEntity appointmentsEntity = findAppointmentById(appointmentDTO.getId());
+        appointmentsEntity.setCustomer(customerEntity);
+        appointmentsEntity.setBeautyProcedure(beautyProceduresEntity);
+        appointmentsEntity.setAppointmentsOpen(false);
+
+        AppointmentsEntity updatedAppointmentsEntity = appointmentRepository.save(appointmentsEntity);
+        return buildAppointmentDTO(updatedAppointmentsEntity);
+    }
+
+    private AppointmentsEntity findAppointmentById(Long id) {
+        return appointmentRepository.findById(id).orElseThrow(() -> new RuntimeException("Appointment not found"));
+    }
+
+    private BeautyProceduresEntity findBeautyProceduresById(Long id) {
+        return beautyProcedureRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Beauty Procedure not found"));
+    }
+
+    private CustomerEntity findCustomerById(Long id) {
+        return customerRepository.findById(id).orElseThrow(() -> new RuntimeException("Customer not found"));
+    }
+
+    private AppointmentDTO buildAppointmentDTO(AppointmentsEntity appointmentsEntity) {
+        return AppointmentDTO.builder()
+                .id(appointmentsEntity.getId())
+                .beautyProcedure(appointmentsEntity.getBeautyProcedure().getId())
+                .dateTime(appointmentsEntity.getDateTime())
+                .appointmentsOpen(appointmentsEntity.getAppointmentsOpen())
+                .customer(appointmentsEntity.getCustomer().getId())
+                .build();
     }
 
 }
